faef74a6297989c83645fe3c7a754717d9378af6
Venkata Akhilesh Pasupuleti <41539204+VenkataAkhileshP@users.noreply.github.com>
2020-04-18 13:37:27 +0530

    Created Simple Server
    Created Simple Server

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
+});
+
+app.listen(3000, () => {
+    console.log(`Server is listening to 3000`);
+});
